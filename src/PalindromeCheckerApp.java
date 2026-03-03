fe40164d1f26bf46c4a8bdc1615e43a83e0f0f5f
HarimitraS <harimitra006@gmail.com>
2026-03-03 15:25:25 +0530

    UC6: Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,37 +6,31 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("==================================");
-        System.out.println("      PALINDROME CHECKER APP      ");
-        System.out.println("==================================");
+        String input = "madam";
 
-        // Define input string
-        String input = "civic";
-
-        // Create Queue (FIFO)
+        Stack<Character> stack = new Stack<>();
         Queue<Character> queue = new LinkedList<>();
 
-        // Create Stack (LIFO)
-        Stack<Character> stack = new Stack<>();
-
-        // Insert characters into both queue and stack
         for (char c : input.toCharArray()) {
-            queue.add(c);      // Enqueue
-            stack.push(c);     // Push
+            stack.push(c);
+            queue.add(c);
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue vs pop
-        while (!queue.isEmpty()) {
-
-            if (!queue.remove().equals(stack.pop())) {
+        for (int i = 0; i < input.length(); i++) {
+            if (stack.pop() != queue.remove()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome");
+        } else {
+            System.out.println(input + " is not a palindrome");
+        }
+
     }
+
 }
